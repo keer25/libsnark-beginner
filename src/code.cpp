c028733361e4c993d851ae947e345f29259c1d9e
Keerthana S <keerukeerthana8@gmail.com>
2018-03-22 11:19:20 +0530

    refactor

---
+++ src/code.cpp
@@ -0,0 +1,53 @@
+#include <libff/common/default_types/ec_pp.hpp>
+#include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
+#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
+
+using namespace libsnark;
+
+template<typename ppT>
+bool run_r1cs_gg_ppzksnark(const r1cs_example<libff::Fr<ppT> > &example)
+{
+    // Setup process with thee generation of keypair using the constrain system which has a verification key and proving key
+    libff::print_header("R1CS GG-ppzkSNARK Generator");
+    r1cs_gg_ppzksnark_keypair<ppT> keypair = r1cs_gg_ppzksnark_generator<ppT>(example.constraint_system);
+    printf("\n"); libff::print_indent(); libff::print_mem("after generator");
+
+    // Verification key used to generate a preprocess verification key which is handed to the party that needs to verify
+    libff::print_header("Preprocess verification key");
+    r1cs_gg_ppzksnark_processed_verification_key<ppT> pvk = r1cs_gg_ppzksnark_verifier_process_vk<ppT>(keypair.vk);
+
+    // primary input are the public parameters of the system and auxilary input are the private parameters of the system
+    libff::print_header("R1CS GG-ppzkSNARK Prover");
+    r1cs_gg_ppzksnark_proof<ppT> proof = r1cs_gg_ppzksnark_prover<ppT>(keypair.pk, example.primary_input, example.auxiliary_input);
+    printf("\n"); libff::print_indent(); libff::print_mem("after prover");
+
+    // The prover can verify the proof using the verification key, the proof and only the public inputs
+    libff::print_header("R1CS GG-ppzkSNARK Verifier");
+    const bool ans = r1cs_gg_ppzksnark_verifier_strong_IC<ppT>(keypair.vk, example.primary_input, proof);
+    printf("\n"); libff::print_indent(); libff::print_mem("after verifier");
+    printf("* The verification result is: %s\n", (ans ? "PASS" : "FAIL"));
+
+    // Verify that the verifier can verify it using just the preprocess verification key without any interaction
+    libff::print_header("R1CS GG-ppzkSNARK Online Verifier");
+    const bool ans2 = r1cs_gg_ppzksnark_online_verifier_strong_IC<ppT>(pvk, example.primary_input, proof);
+    assert(ans == ans2);
+
+    return ans;
+}
+
+template<typename ppT>
+void test_r1cs_gg_ppzksnark(size_t num_constraints, size_t input_size)
+{
+    r1cs_example<libff::Fr<ppT> > example = generate_r1cs_example_with_binary_input<libff::Fr<ppT> >(num_constraints, input_size);
+    const bool bit = run_r1cs_gg_ppzksnark<ppT>(example);
+    assert(bit);
+}
+
+int main () {
+    default_r1cs_gg_ppzksnark_pp::init_public_params();
+    // Calling a contraint system with 500 contraints and 50 input size
+    test_r1cs_gg_ppzksnark<default_r1cs_gg_ppzksnark_pp>(500, 50);
+
+    return 0;
+}
